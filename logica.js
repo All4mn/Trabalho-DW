f9c93fb3aeae94a5deaacc0a45a19c214ad56066
Dedei-AB <andreiabners@gmail.com>
2024-11-29 14:28:04 -0300

    rascunho js

--- logica.js
+++ logica.js
@@ -1,9 +1,15 @@
 let inputNT = document.getElementById("nomeTarefa")
 let buttonAdd = document.getElementById("adicionar")
 let inputTask = document.getElementsByClassName("tarefa")
+let checkbox = document.getElementById("conclusao")
+
+let tarefas = [
+    {nome: "inputNT", conclusão: false},
+    {nome: "Tarefa de Dw", conclusão: false}
+]
+
 function criarTarefa(){
-    let div 
-    div = document.createElement("tarefa")
+    let div = document.createElement("div.tarefa")
     div.nome = inputNT
 }
 buttonAdd.onclick = criarTarefa
