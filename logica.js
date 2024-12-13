0ed92f4182d2d6e0f327c0cd520e9f7299d6af2e
Dedei-AB <andreiabners@gmail.com>
2024-12-13 10:21:54 -0300

    dfghjk

--- logica.js
+++ logica.js
@@ -26,13 +26,9 @@ function listTarefa(lista) {
     let novaTarefa = document.createElement("div");
     novaTarefa.classList.add("tarefa");
 
-    novaTarefa.innerHTML = `<input type="checkbox" id="conclusao${index}" ${
-      tarefa.conclusao ? "checked" : ""
-    }>
+    novaTarefa.innerHTML = `<input type="checkbox" id="conclusao${index}" ${tarefa.conclusao ? "checked" : ""}>
         <p id="nomeTarefa${index}">${tarefa.nome}</p>
-        <input type="text" class="nomeTarefa" id="editTarefa${index}" value="${
-      tarefa.nome
-    }" style="display: none;">
+        <input type="text" class="nomeTarefa" id="editTarefa${index}" value="${tarefa.nome}" style="display: none;">
         <button class="salvar" id="salvar${index}" style="display: none;">Salvar</button>
         <div class="edicao">
             <svg class="lixo" id="lixo${index}" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
